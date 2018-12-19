606bf322734882130597175fdddbf7fb095e72e2
Will Smythe <wismythe@microsoft.com>
2018-12-19 14:33:12 -0500

    Terminal and color tests

---
+++ index.js
@@ -0,0 +1,64 @@
+console.log('Before require');
+
+const chalk = require('chalk');
+const log = console.log;
+
+log('start');
+
+log(`enabled: ${chalk.enabled}`);
+log(`level: ${chalk.level}`);
+log(`supportsColor: ${JSON.stringify(chalk.supportsColor)}`);
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+const cpu = {
+    totalPercent: .9
+};
+
+const ram = {
+    used: 100,
+    total: 300
+};
+
+const disk = {
+    used: 100,
+    total: 300
+};
+
+// ES2015 tagged template literal
+log(chalk`
+CPU: {red ${cpu.totalPercent}%}
+RAM: {green ${ram.used / ram.total * 100}%}
+DISK: {rgb(255,131,0) ${disk.used / disk.total * 100}%}
+`);
+
+// Use RGB colors in terminal emulators that support it.
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
+
+log('start');
