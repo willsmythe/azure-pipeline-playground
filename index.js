ae3253bb1e8b3e77eb63fe582f5ca521e49c450c
Will Smythe <wismythe@microsoft.com>
2019-01-08 07:59:14 -0500

    More debug

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const realpath = require('realpath-native');
+const os = require('os');
 
 console.log(`cwd: ${process.cwd()}`);
 console.log(`realpath: ${realpath.sync(process.cwd())}`);
+
+console.log(`tmpdir: ${os.tmpdir()}`);
+console.log(`realpath: ${realpath.sync(os.tmpdir())}`);
